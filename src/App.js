cd9f40df3f08b6d9cd79f4d9e56d165e9c22d968
Kateryna Sosnikova <sosnikova@gmail.com>
2021-01-08 10:52:04 +0200

    made it accarute

--- src/App.js
+++ src/App.js
@@ -17,79 +17,22 @@ const sensorCreate = (name) => {
   return of(undefined).pipe(
     delay(intervalRandom(100, 1700)),
     mapTo(`${name}: ${randomNumber(100)}`),
-    //mapTo({ name, value: randomNumber(1000) }),
     timeout(1500),
     catchError(err => of(undefined).pipe(mapTo(`${name}: No data`))),
-    //catchError(err => of(undefined).pipe(mapTo({ name, value: 'No data' }))),
     tap(ev => console.log(ev))
   )
 };
 
-// const sensorA$ = of(undefined).pipe(
-//   expand(() => of(undefined).pipe(
-//     mapTo(`sensorD: ${randomNumber(100)} `),
-//     delay(intervalRandom(100, 1500)),
-//     timeout(1500),
-//     catchError(err => of(undefined).pipe(mapTo('sensorD No data'))),
-//     tap(ev => console.log(ev))
-//   ))
-// )
-
-// const allSensors$ = of(undefined).pipe(expand(() => {     
-//     return 
-// merge(sensorCreate('A'), sensorCreate('B'), sensorCreate('C'), sensorCreate('D'))
-// .pipe(delay(200),
-//    // tap(e => console.log(e)),
-//     //map((sensors, curr) => ({ ...sensors, [curr.name]: curr.value }), {}),
-//     //scan((state, curr) => ([ ...state, ...curr ]), []),
-//     tap(el => console.log(`Last sensor emited value ${el}`))
-// );
-// }))
-
-const repeater$ = interval(500);
-const any$ = combineLatest(sensorCreate('A'), sensorCreate('B'), sensorCreate('C'), sensorCreate('D'));
-
-// const allSensors$ = zip(sensorCreate('A'), sensorCreate('B'), sensorCreate('C'), sensorCreate('D'))
-//           .pipe(expand(() => any$.pipe(
-//             delay(200),
-//             tap(i => console.log(i)))))
+const allSensors$ = of(undefined).pipe(
+  expand(() => zip(
+    sensorCreate('A'), sensorCreate('B'),
+    sensorCreate('C'), sensorCreate('D')
+  )))
 
 
 function App() {
 
   const [state, setState] = useState(null);
-
-  const sensors$ = merge(sensorCreate('A'), sensorCreate('B'),
-    sensorCreate('C'), sensorCreate('D'));
-  const sensorsStart$ = zip(sensorCreate('A'), sensorCreate('B'),
-    sensorCreate('C'), sensorCreate('D'));
-
-  // const allSensors$ = of(undefined)
-  // .pipe(expand(() => {return sensorsStart$}),
-  //   delay(200),
-  //   // startWith(() => { return of(undefined).
-  //   //  pipe(expand(() => sensorsStart$))}),
-  //   tap(i => console.log(i)),
-  //   scan((state, curr) => ({ ...state, ...curr }), {})
-  //   )
-
-
-  const allSensors$ = of(undefined).pipe(
-    expand(() => zip(
-      sensorCreate('A'), sensorCreate('B'),
-      sensorCreate('C'), sensorCreate('D')
-    )
-      // .pipe(
-      //   tap(i => console.log(i)),
-      //  // delay(200),
-      //   // switchMap(() => { return of(undefined).
-      //   //   pipe(expand(() => sensors$))}),
-      //   // expand(() => sensors$.pipe(
-      //   // tap(i => console.log(i)),
-      //   // scan((state, curr) => ({ ...state, ...curr }), {}),
-      //   // repeat(true)
-      // )
-    ))
 
   useEffect(() => {
     const sub = allSensors$.subscribe(setState);
